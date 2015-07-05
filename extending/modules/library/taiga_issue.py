d6d22e8c3122070c90e2fba1abb68f4dba323b67
Alejandro Guirao <lekumberri@gmail.com>
2015-07-05 18:27:49 +0200

    Corrections

--- extending/modules/library/taiga_issue.py
+++ extending/modules/library/taiga_issue.py
@@ -26,7 +26,7 @@ description:
   - Creates/deletes an issue in a Taiga Project Management Platform (U(https://taiga.io)).
   - An issue is identified by the combination of project, issue subject and issue type.
   - This module implements the creation or deletion of issues (not the update).
-version_added: "1.9"
+version_added: "2.0"
 options:
   taiga_host:
     description:
@@ -59,7 +59,7 @@ options:
     description:
       - The issue severity. Must exist previously.
     required: False
-    default: New
+    default: Normal
   description:
     description:
       - The issue description.
